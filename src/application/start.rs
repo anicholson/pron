722eaadb663a86ca051a937f25e84b1228494ee7
Andy Nicholson <andrew@anicholson.net>
2026-06-30 21:37:34 +1000

    auto(ses_0e8c): edit src/application/start.rs
    Session: ses_0e8c97cb0ffegmE4tkUsjRVLTj
    Agent: opencode
    Model: opencode-go/glm-5.2

--- src/application/start.rs
+++ src/application/start.rs
@@ -23,10 +23,10 @@ impl<U: Filesystem, L: Logger, P: ProcessControl> Start<U, L, P> {
 
 #[cfg(test)]
 mod tests {
-    use super::Start;
     use crate::application::ports::filesystem::in_memory::InMemoryFilesystem;
     use crate::application::ports::logger::in_memory::InMemoryLogger;
     use crate::application::ports::process_control::in_memory::InMemoryProcessControl;
+    use crate::application::start::Start;
 
     mod execute {
         mod when_called_with_a_valid_crontab_and_daemon_mode {
