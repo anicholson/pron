a95929496ddeba08e074db2a5951e15703d26b93
Andy Nicholson <andrew@anicholson.net>
2026-07-01 21:21:58 +1000

    auto(ses_0e8c): write tests/port_clock.rs
    Session: ses_0e8c97cb0ffegmE4tkUsjRVLTj
    Agent: opencode
    Model: opencode-go/glm-5.2

--- tests/port_clock.rs
+++ tests/port_clock.rs
@@ -1,14 +1,41 @@
 #[macro_export]
 macro_rules! clock_contract {
-    ($make:expr) => {
-        mod clock_contract {
+    ($mod_name:ident, $make:expr) => {
+        mod $mod_name {
+            use super::*;
+
             mod now {
                 mod when_called {
                     #[test]
                     fn then_the_current_minute_tuple_is_returned() {
                         use pron::application::ports::clock::Clock;
                         let clock = $make;
-                        let _m = clock.now();
+                        let m = clock.now();
+                        assert!(
+                            m.min <= 59,
+                            "minute should be in 0..=59, got {}",
+                            m.min
+                        );
+                        assert!(
+                            m.hour <= 23,
+                            "hour should be in 0..=23, got {}",
+                            m.hour
+                        );
+                        assert!(
+                            m.dom >= 1 && m.dom <= 31,
+                            "day-of-month should be in 1..=31, got {}",
+                            m.dom
+                        );
+                        assert!(
+                            m.mon >= 1 && m.mon <= 12,
+                            "month should be in 1..=12, got {}",
+                            m.mon
+                        );
+                        assert!(
+                            m.dow <= 6,
+                            "day-of-week should be in 0..=6, got {}",
+                            m.dow
+                        );
                     }
                 }
             }
@@ -16,4 +43,5 @@ macro_rules! clock_contract {
     };
 }
 
-clock_contract!(pron::application::ports::clock::in_memory::InMemoryClock::with(0, 0, 1, 1, 0));
+clock_contract!(in_memory, pron::application::ports::clock::in_memory::InMemoryClock::with(30, 14, 15, 7, 2));
+clock_contract!(real, pron::adapters::clock::SystemClock::new());
