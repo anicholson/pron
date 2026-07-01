1c8be0658a750e028c52e8b4ad30e1a3f58f307b
Andy Nicholson <andrew@anicholson.net>
2026-07-01 21:22:54 +1000

    auto(ses_0e8c): write tests/port_clock.rs
    Session: ses_0e8c97cb0ffegmE4tkUsjRVLTj
    Agent: opencode
    Model: opencode-go/glm-5.2

--- tests/port_clock.rs
+++ tests/port_clock.rs
@@ -2,8 +2,6 @@
 macro_rules! clock_contract {
     ($mod_name:ident, $make:expr) => {
         mod $mod_name {
-            use super::*;
-
             mod now {
                 mod when_called {
                     #[test]
@@ -11,31 +9,11 @@ macro_rules! clock_contract {
                         use pron::application::ports::clock::Clock;
                         let clock = $make;
                         let m = clock.now();
-                        assert!(
-                            m.min <= 59,
-                            "minute should be in 0..=59, got {}",
-                            m.min
-                        );
-                        assert!(
-                            m.hour <= 23,
-                            "hour should be in 0..=23, got {}",
-                            m.hour
-                        );
-                        assert!(
-                            m.dom >= 1 && m.dom <= 31,
-                            "day-of-month should be in 1..=31, got {}",
-                            m.dom
-                        );
-                        assert!(
-                            m.mon >= 1 && m.mon <= 12,
-                            "month should be in 1..=12, got {}",
-                            m.mon
-                        );
-                        assert!(
-                            m.dow <= 6,
-                            "day-of-week should be in 0..=6, got {}",
-                            m.dow
-                        );
+                        assert!(m.min <= 59, "minute should be in 0..=59, got {}", m.min);
+                        assert!(m.hour <= 23, "hour should be in 0..=23, got {}", m.hour);
+                        assert!(m.dom >= 1 && m.dom <= 31, "day-of-month should be in 1..=31, got {}", m.dom);
+                        assert!(m.mon >= 1 && m.mon <= 12, "month should be in 1..=12, got {}", m.mon);
+                        assert!(m.dow <= 6, "day-of-week should be in 0..=6, got {}", m.dow);
                     }
                 }
             }
