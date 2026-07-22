772144d3aa606b85753cc07ec8bfa96455b84193
Andy Nicholson <andrew@anicholson.net>
2026-07-22 17:20:15 +1000

    auto(ses_07c0): edit tests/port_process_control.rs
    Session: ses_07c022345ffeWrM8mlnhf87vb7
    Agent: opencode
    Model: opencode-go/kimi-k3

--- tests/port_process_control.rs
+++ tests/port_process_control.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 macro_rules! process_control_contract {
-    ($mod_name:ident, $make:expr, $expected:expr) => {
+    ($mod_name:ident, $make:expr, $expected:expr $(, $live_pron_make:expr)?) => {
         mod $mod_name {
             mod current_pid {
                 mod when_called {
@@ -12,6 +12,26 @@ macro_rules! process_control_contract {
                     }
                 }
             }
+            mod is_live_pron {
+                mod when_called_with_a_pid_that_is_not_a_live_pron_process {
+                    #[test]
+                    fn then_false_is_returned() {
+                        use pron::application::ports::process_control::ProcessControl;
+                        let pc = $make;
+                        assert!(!pc.is_live_pron(999999));
+                    }
+                }
+                $(
+                mod when_called_with_a_configured_live_pron_pid {
+                    #[test]
+                    fn then_true_is_returned() {
+                        use pron::application::ports::process_control::ProcessControl;
+                        let pc = $live_pron_make;
+                        assert!(pc.is_live_pron(4242));
+                    }
+                }
+                )?
+            }
         }
     };
 }
@@ -19,7 +39,8 @@ macro_rules! process_control_contract {
 process_control_contract!(
     in_memory,
     pron::application::ports::process_control::in_memory::InMemoryProcessControl::with_pid(4242),
-    4242u32
+    4242u32,
+    pron::application::ports::process_control::in_memory::InMemoryProcessControl::with_live_pron(4242)
 );
 
 process_control_contract!(
